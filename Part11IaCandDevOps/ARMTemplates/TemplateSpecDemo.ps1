682e63afeb84a334ca04069a56d4ae8ac9a412a0
John Savill <john@savilltech.com>
2020-12-20 07:07:31 -0600

    Template specs updates

---
+++ Part11IaCandDevOps/ARMTemplates/TemplateSpecDemo.ps1
@@ -0,0 +1,43 @@
+$GitBasePath = 'C:\Users\john\OneDrive\projects\GIT\AzureMasterClass\Part11IaCandDevOps\ARMTemplates'
+$RGName = '$RGName'
+
+#Create a new template spec
+New-AzTemplateSpec -Name Basic-Storage-Account -Version 1.0 -ResourceGroupName RG-TemplateSpecs -Location southcentralus `
+    -TemplateFile "$GitBasePath\StorageAccount.json"
+
+Get-AzTemplateSpec
+
+Get-AzTemplateSpec -ResourceGroupName RG-TemplateSpecs -Name Basic-Storage-Account
+
+#Get the template spec ID for a version then deploy
+$id = (Get-AzTemplateSpec -ResourceGroupName RG-TemplateSpecs -Name Basic-Storage-Account -Version 1.0).Versions.Id
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateSpecId $id
+
+#with parameter file
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateSpecId $id `
+    -TemplateParameterFile "$GitBasePath\StorageAccount.parameters.json" `
+    -WhatIf
+
+#Deploying a linked template today (need public accessible path)
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile "$GitBasePath\StorageAccountLinkedGitHub.json" `
+    -TemplateParameterFile "$GitBasePath\StorageAccount.parameters.json" `
+    -WhatIf
+
+#Create template spec from file with local link
+New-AzTemplateSpec -Name Linked-Storage-Account -Version 1.0 -ResourceGroupName RG-TemplateSpecs -Location southcentralus `
+    -TemplateFile "$GitBasePath\StorageAccountLinkedLocal.json"
+
+$id = (Get-AzTemplateSpec -ResourceGroupName RG-TemplateSpecs -Name Linked-Storage-Account -Version 1.0).Versions.Id
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateSpecId $id -WhatIf
+
+
+#Deploy linking to a template spec from local template (i.e. using template spec as a module within a template)
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile "$GitBasePath\StorageAccountLinkedTempSpec.json" -WhatIf
+
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile "$GitBasePath\StorageAccountLinkedTempSpec.json" `
+    -TemplateParameterFile "$GitBasePath\StorageAccount.parameters.json" `
+    -WhatIf
+
+#Template spec creation that uses another template spec
+New-AzTemplateSpec -Name SpecLinked-Storage-Account -Version 1.0 -ResourceGroupName RG-TemplateSpecs -Location southcentralus `
+    -TemplateFile "$GitBasePath\StorageAccountLinkedTempSpec.json"
