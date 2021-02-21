8e796a80ec53960548d6211328ce320940a4e88c
John Savill <john@savilltech.com>
2021-02-21 11:19:18 -0600

    Added community BGPs to module 6

--- Part06Networking/VNetNSGEndpoint.ps1
+++ Part06Networking/VNetNSGEndpoint.ps1
@@ -5,5 +5,9 @@ $serviceTags.Values | Where-Object {$_.Name -eq "Storage.SouthCentralUS"}
 
 ($serviceTags.Values | Where-Object {$_.Name -eq "SQL.SouthCentralUS"}).Properties.AddressPrefixes
 
+#These are the BGP Communities
+$BGPCommunity = Get-AzBgpServiceCommunity
+$BGPCommunity | Where-Object {$_.Name -eq "AzureStorageSouthCentralUS"}
+
 #lookup against Azure DNS default private zone
 nslookup savazuusscds01.internal.cloudapp.net 168.63.129.16
