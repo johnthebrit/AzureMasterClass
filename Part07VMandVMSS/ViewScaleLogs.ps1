ee32cb3108661058b8ba797a7958da6d0ad002ca
John Savill <john@savilltech.com>
2023-01-22 13:14:14 -0600

    Module 7 handout

--- Part07VMandVMSS/ViewScaleLogs.ps1
+++ Part07VMandVMSS/ViewScaleLogs.ps1
@@ -4,8 +4,6 @@ $VMSSResourceID =  $VMSSResource.Id
 $VMSSResource | Format-List
 $VMSSResource.VirtualMachineProfile #see the profile
 
-Start-AzVmss -VMScaleSetName 'VMSSFlex' -ResourceGroupName RG-VMSS
-
 $alllogs = Get-AzLog -DetailedOutput -ResourceId $VMSSResourceID -StartTime (Get-Date).AddDays(-7) -EndTime (Get-Date) -WarningAction SilentlyContinue
 $scalelogs = $alllogs | Where-Object {$_.OperationName -eq "Autoscale scale down completed" -or
             $_.OperationName -eq "Autoscale scale up completed"}
@@ -14,4 +12,6 @@ $scalelogs = $scalelogs | Sort-Object -Property EventTimestamp
 $scalelogs |
     select-object EventTimestamp, OperationName, @{Name='OldCount';Expression={$_.properties.content.OldInstancesCount}},@{Name='NewCount';Expression={$_.properties.content.NewInstancesCount}}
 
+#Control
+Start-AzVmss -VMScaleSetName 'VMSSFlex' -ResourceGroupName RG-VMSS
 Stop-AzVmss -VMScaleSetName 'VMSSFlex' -ResourceGroupName RG-VMSS #cannot do if ephemeral
